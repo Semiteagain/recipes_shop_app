1efdd66c61f2000dd1d7e4f33de40d1f651465a9
Semiu Mukaila <semiteagain@gmail.com>
2023-10-12 19:41:43 +0100

    card

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -11,29 +11,53 @@ class ExplorePages extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       children: [
         // Filter and Search Field
-        Row(
-          children: [
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: 'Search here...',
-                    isDense: true,
-                    contentPadding: const EdgeInsets.all(8),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey.shade300),
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(99),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 12.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Search here...',
+                      isDense: true,
+                      contentPadding: const EdgeInsets.all(8),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey.shade300),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(99),
+                        ),
                       ),
-                    ),
-                    prefixIcon: const Icon(IconlyLight.search)),
+                      prefixIcon: const Icon(IconlyLight.search)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: IconButton.filled(
+                  onPressed: () {},
+                  icon: const Icon(IconlyLight.filter),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 170,
+          child: Card(
+            color: Colors.green.shade50,
+            elevation: 0.1,
+            shadowColor: Colors.green.shade50,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  Column(
+                    children: [Text("Free Consultation")],
+                  )
+                ],
               ),
             ),
-            IconButton.filled(
-              onPressed: () {},
-              icon: const Icon(IconlyLight.filter),
-            )
-          ],
-        )
+          ),
+        ),
       ],
     ));
   }
