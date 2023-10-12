3226971d9cf39e23e2483bd1ba10ff9689553370
Semiu Mukaila <semiteagain@gmail.com>
2023-10-12 03:17:55 +0100

    add explore page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,44 +21,48 @@ class _HomePageState extends State<HomePage> {
     const ProfilePage()
   ];
 
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: const Drawer(),
-        appBar: AppBar(
-          centerTitle: false,
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Hi Bash 👏',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              Text(
-                'Enjoy our services',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-            ],
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: IconButton.filledTonal(
-                onPressed: () {},
-                icon: badges.Badge(
-                  badgeContent: const Text(
-                    '3',
-                    style: TextStyle(color: Colors.white, fontSize: 12),
-                  ),
-                  badgeStyle: const badges.BadgeStyle(
-                    badgeColor: Colors.green,
-                  ),
-                  position: badges.BadgePosition.topEnd(top: -15, end: -12),
-                  child: const Icon(IconlyBroken.notification),
-                ),
-              ),
+      drawer: const Drawer(),
+      appBar: AppBar(
+        centerTitle: false,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hi Bash 👏',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Text(
+              'Enjoy our services',
+              style: Theme.of(context).textTheme.bodySmall,
             ),
           ],
-        ));
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton.filledTonal(
+              onPressed: () {},
+              icon: badges.Badge(
+                badgeContent: const Text(
+                  '3',
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+                badgeStyle: const badges.BadgeStyle(
+                  badgeColor: Colors.green,
+                ),
+                position: badges.BadgePosition.topEnd(top: -15, end: -12),
+                child: const Icon(IconlyBroken.notification),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: pages[currentIndex],
+    );
   }
 }
