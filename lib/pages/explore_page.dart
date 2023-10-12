949b07bea792caeaff7a6c854716e0ef9e2e6f0b
Semiu Mukaila <semiteagain@gmail.com>
2023-10-12 04:18:00 +0100

    decorating the text field area

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 
 class ExplorePages extends StatelessWidget {
   const ExplorePages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Explore'),
-      ),
-    );
+    return Scaffold(
+        body: ListView(
+      padding: const EdgeInsets.all(16),
+      children: [
+        // Filter and Search Field
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Search here...',
+                    isDense: true,
+                    contentPadding: const EdgeInsets.all(8),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade300),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(99),
+                      ),
+                    ),
+                    prefixIcon: const Icon(IconlyLight.search)),
+              ),
+            ),
+            IconButton.filled(
+              onPressed: () {},
+              icon: const Icon(IconlyLight.filter),
+            )
+          ],
+        )
+      ],
+    ));
   }
 }
